4bdbdc243b4b86aa459a642ec30393e83193b3f0
ayibrahim1 <ibrahiay@mail.uc.edu>
2022-01-08 17:39:40 -0500

    Created Project in IntelliJ

---
+++ src/test/java/com/enterprise/eventmanagementsystem/EventmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.eventmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

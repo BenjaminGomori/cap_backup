8fdfd4e1b2a1a93c397d7f68e3012f0197ee9a35
ayibrahim1 <ibrahiay@mail.uc.edu>
2022-02-03 10:48:53 -0500

    Built Interface IEventService for the project

--- src/main/java/com/enterprise/eventmanagementsystem/service/IEventService.java
+++ src/main/java/com/enterprise/eventmanagementsystem/service/IEventService.java
@@ -1,4 +1,14 @@
 package com.enterprise.eventmanagementsystem.service;
+import com.enterprise.eventmanagementsystem.dto.Event;
 
 public interface IEventService {
+
+
+    Iterable<Event> fetchAll() throws Exception;
+
+    Event save(Event event) throws Exception;
+
+    Event fetch(int id);
+
+    void delete(int id) throws Exception;
 }

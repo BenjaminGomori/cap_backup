bc5ad77b6fe91c3cd9b91fdaf498a3f7bd328224
ayibrahim1 <ibrahiay@mail.uc.edu>
2022-02-03 10:47:43 -0500

    Built EventService for the project
    Need to add a search feature to the project.

--- src/main/java/com/enterprise/eventmanagementsystem/service/EventService.java
+++ src/main/java/com/enterprise/eventmanagementsystem/service/EventService.java
@@ -1,4 +1,44 @@
 package com.enterprise.eventmanagementsystem.service;
+import com.enterprise.eventmanagementsystem.dao.EventRepository;
+import com.enterprise.eventmanagementsystem.dao.IEventDAO;
+import com.enterprise.eventmanagementsystem.dto.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.stereotype.Service;
 
-public class EventService {
+@Service
+public class EventService implements IEventService {
+
+
+    @Autowired
+    IEventDAO eventDAO;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public EventService(IEventDAO eventDAO)
+    {
+        this.eventDAO = eventDAO;
+    }
+
+    @Override
+    public Iterable<Event> fetchAll() throws Exception {
+        return eventDAO.fetchAll();
+    }
+
+    @Override
+    public Event save(Event event) throws Exception {
+        return eventDAO.save(event);
+    }
+
+    @Override
+    public Event fetch(int id) {
+        return eventDAO.fetch(id);
+    }
+
+    @Override
+    @CacheEvict(value = "delete", key = "#id")
+    public void delete(int id) {
+        this.eventRepository.deleteById(id);
+    }
 }

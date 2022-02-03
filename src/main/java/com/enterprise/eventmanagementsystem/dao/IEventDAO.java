1b0bcc3bc869c8989b33ad73e420a4549129c6ed
ayibrahim1 <ibrahiay@mail.uc.edu>
2022-02-03 10:42:36 -0500

    Built Interface IEventDAO for the project

--- src/main/java/com/enterprise/eventmanagementsystem/dao/IEventDAO.java
+++ src/main/java/com/enterprise/eventmanagementsystem/dao/IEventDAO.java
@@ -1,4 +1,8 @@
 package com.enterprise.eventmanagementsystem.dao;
+import com.enterprise.eventmanagementsystem.dto.Event;
 
 public interface IEventDAO {
+    Event fetch(int eventId);
+    Iterable<Event> fetchAll() throws Exception;
+    Event save(Event event) throws Exception;
 }

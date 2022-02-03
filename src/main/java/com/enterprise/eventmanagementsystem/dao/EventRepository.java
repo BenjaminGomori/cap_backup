2edb54f95e70d745d18ddc797f164b7d8e8c0b67
ayibrahim1 <ibrahiay@mail.uc.edu>
2022-02-03 10:39:10 -0500

    Built EventRepository for the project

--- src/main/java/com/enterprise/eventmanagementsystem/dao/EventRepository.java
+++ src/main/java/com/enterprise/eventmanagementsystem/dao/EventRepository.java
@@ -1,4 +1,9 @@
 package com.enterprise.eventmanagementsystem.dao;
+import com.enterprise.eventmanagementsystem.dto.Event;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface EventRepository {
+@Repository("EventRepository")
+public interface EventRepository extends CrudRepository<Event, Integer> {
+
 }

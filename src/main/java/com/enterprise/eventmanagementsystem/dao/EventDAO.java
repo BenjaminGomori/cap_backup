984d6a79964a10961e92ce5cf8a015c3e531a12e
ayibrahim1 <ibrahiay@mail.uc.edu>
2022-02-03 10:37:32 -0500

    Built EventDAO layer for the project

--- src/main/java/com/enterprise/eventmanagementsystem/dao/EventDAO.java
+++ src/main/java/com/enterprise/eventmanagementsystem/dao/EventDAO.java
@@ -1,4 +1,32 @@
 package com.enterprise.eventmanagementsystem.dao;
+import com.enterprise.eventmanagementsystem.dto.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
-public class EventDAO {
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventDAO implements IEventDAO {
+
+    Map<Integer, Event> allEvents = new HashMap<>();
+
+    @Autowired
+    @Qualifier("eventRepository")
+    EventRepository eventRepository;
+
+    @Override
+    public Event fetch(int eventId) {
+        return eventRepository.findById(eventId).get();
+    }
+
+    @Override
+    public Iterable<Event> fetchAll() throws Exception {
+        return eventRepository.findAll();
+    }
+
+    @Override
+    public Event save(Event event) throws Exception {
+        Event save = eventRepository.save(event);
+        return save;
+    }
 }

087664fd2905beece268c65a33cd647ed50614cc
ayibrahim1 <ibrahiay@mail.uc.edu>
2022-02-03 10:50:30 -0500

    Update EventmanagementsystemApplication.java
    This will help the application run properly.

--- src/main/java/com/enterprise/eventmanagementsystem/EventmanagementsystemApplication.java
+++ src/main/java/com/enterprise/eventmanagementsystem/EventmanagementsystemApplication.java
@@ -1,9 +1,13 @@
 package com.enterprise.eventmanagementsystem;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableCaching
 public class EventmanagementsystemApplication {
 
     public static void main(String[] args) {

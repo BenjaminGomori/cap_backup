92bc23753bdd971ef16b106d5324b50ef9db4a1b
benjamin gomori <benjamingomori@gmail.com>
2022-03-07 23:18:19 -0500

    Renames file and removes console.logs

--- src/main/resources/static/js/datetime.js
+++ src/main/resources/static/js/datetime.js
@@ -2,17 +2,10 @@ const timeControl = document.querySelector('input[type="datetime-local"]');
 const timeControlHidden = document.querySelector('.hid');
 
 window.onload = (event) => {
-    console.log('page is fully loaded');
-    console.log(timeControlHidden.value);
-    console.log(timeControlHidden.value.replace(' ', 'T'));
     timeControl.value = timeControlHidden.value.replace(' ', 'T');
 };
 
 timeControl.addEventListener('change', (event) => {
-    console.log(timeControl);
-    console.log(timeControl.value);
     let date = new Date(timeControl.value);
-    console.log(date.toISOString().slice(0, 19).replace('T', ' '));
     timeControlHidden.value = date.toISOString().slice(0, 19).replace('T', ' ');
-    console.log(timeControlHidden.value);
 });

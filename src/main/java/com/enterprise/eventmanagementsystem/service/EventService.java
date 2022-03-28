19590fada533d410b5e283c2e269669c73eef74d
zhukv98 <76707463+zhukv98@users.noreply.github.com>
2022-03-28 16:24:42 -0400

    Reformatted code and fixed the js directory.

--- src/main/java/com/enterprise/eventmanagementsystem/service/EventService.java
+++ src/main/java/com/enterprise/eventmanagementsystem/service/EventService.java
@@ -1,4 +1,5 @@
 package com.enterprise.eventmanagementsystem.service;
+
 import com.enterprise.eventmanagementsystem.dao.EventRepository;
 import com.enterprise.eventmanagementsystem.dao.IEventDAO;
 import com.enterprise.eventmanagementsystem.dto.Event;
@@ -14,6 +15,7 @@ import java.util.List;
  * The application's border is defined by this class.
  * From the standpoint of interfacing client layers, it is a set of possible operations.
  * It contains the business logic of the application, controls transactions, and coordinates replies in the execution of its operations.
+ *
  * @author Ali Ibrahim, Benjamin Gomori, Kevin Zhu, and Son Bo
  */
 @Service
@@ -26,8 +28,7 @@ public class EventService implements IEventService {
     @Autowired
     EventRepository eventRepository;
 
-    public EventService(IEventDAO eventDAO)
-    {
+    public EventService(IEventDAO eventDAO) {
         this.eventDAO = eventDAO;
     }
 
@@ -51,13 +52,14 @@ public class EventService implements IEventService {
     public void delete(int id) {
         this.eventRepository.deleteById(id);
     }
+
     @Override
-    public List<LabelValue> generateEventsSearchableData() throws Exception{
-        List<LabelValue> eventsSearchableData =  new LinkedList<>();
-        for (Event event: fetchAll()) {
+    public List<LabelValue> generateEventsSearchableData() throws Exception {
+        List<LabelValue> eventsSearchableData = new LinkedList<>();
+        for (Event event : fetchAll()) {
             LabelValue labelValue = new LabelValue();
             labelValue.setValue(event.getId());
-            labelValue.setLabel(event.getName() + ", "+ event.getDescription());
+            labelValue.setLabel(event.getName() + ", " + event.getDescription());
             eventsSearchableData.add(labelValue);
         }
         return eventsSearchableData;
@@ -65,9 +67,9 @@ public class EventService implements IEventService {
 
     @Override
     public List<Event> searchEvents(String searchText) throws Exception {
-        List<Event> eventList =  new LinkedList<>();
-        for (Event event: fetchAll()) {
-            if(event.getName().contains(searchText) || event.getDescription().contains(searchText)){
+        List<Event> eventList = new LinkedList<>();
+        for (Event event : fetchAll()) {
+            if (event.getName().contains(searchText) || event.getDescription().contains(searchText)) {
                 eventList.add(event);
             }
         }

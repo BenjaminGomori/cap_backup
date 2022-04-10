533ce243d03d6d79af7591b1841cb1f1f5f9f98d
benjamin gomori <benjamingomori@gmail.com>
2022-04-10 17:06:16 -0400

    Adding delete to DAOs and adjusting one DAO to be a stub.

--- src/main/java/com/enterprise/eventmanagementsystem/dao/EventSQLDAO.java
+++ src/main/java/com/enterprise/eventmanagementsystem/dao/EventSQLDAO.java
@@ -1,10 +1,12 @@
 package com.enterprise.eventmanagementsystem.dao;
 import com.enterprise.eventmanagementsystem.dto.Event;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 
 @Repository("eventDAO")
+@Profile({"dev", "default"})
 public class EventSQLDAO implements IEventDAO {
 
 
@@ -24,5 +26,10 @@ public class EventSQLDAO implements IEventDAO {
     @Override
     public Event save(Event event) throws Exception {
         return eventRepository.save(event);
+    }
+
+    @Override
+    public void deleteById(int id) throws Exception {
+        eventRepository.deleteById(id);
     }
 }
